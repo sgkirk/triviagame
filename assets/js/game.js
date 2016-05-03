234b1eef5296265f5d731f85b5a2ae9de8f1c065
Suzanne Kirk <sgraykirk@gmail.com>
2016-05-03 15:31:05 -0400

    JS updates, more HTML

---
+++ assets/js/game.js
@@ -0,0 +1,7 @@
+function unhide(questions) {
+    var item = document.getElementById(questions);
+    if (item) {
+            item=(item=='hidden')?'unhidden':'hidden';
+        }
+       // document.getElementById(startbutton) = 'hidden';
+    }
