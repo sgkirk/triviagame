e8b5c6f425b1d5eb6f6bfe0fdb8544ddd99468e2
Suzanne Kirk <sgraykirk@gmail.com>
2016-05-04 12:43:28 -0400

    js updates

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,31 +1,30 @@
 $(document).ready(function(){
 
-$('#questions').hide();
 $('#submitbutton').hide();
 
-	// var questions = [
-	//      {
-	//      	question: "question 1",
-	//      	answer1: "answer 1",
-	//      	answer2: "answer 2"
-	//      },
-	//      {
-	//      	question2: "question 2",
-	//      	answer1: "answer 1",
-	//      	answer2: "answer 2",
-	//      }
-
-	// ]     
+var questions = [
+	{
+	question1: "This is question one",
+	answer1: "this is answer one yay",
+	answer2: "answer 2",
+	},
+	{
+	question2: "question 2",
+	answer1: "answer 1",
+	answer2: "answer 2",
+	}
+	]     
+    
 
     $('#startbutton').on('click', function () {
-    		//$('#questions').append('something');
+    		$('#question').append(questions[0].question1);
+    		$('#answer1').append(questions[0].answer1);
     		$('#startbutton').hide();
-    		$('#questions').show();
     		$('#submitbutton').show();
 
     })
 
 
-
+//end of document.ready function
 })
 
