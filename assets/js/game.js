46d031f7150406591f40de7235f67fab30b650a2
Suzanne Kirk <sgraykirk@gmail.com>
2016-05-04 16:47:17 -0400

    getting closer

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,21 +1,28 @@
 $(document).ready(function(){
 
-$('#submitbutton').hide();
+var userScore = {
+correct: 0,
+incorrect: 0, 
+} 
+
+$('#nextbutton1').hide();
 $('#answer1').hide();
 $('#answer2').hide();
 $('#answer3').hide();
 
 var questions = [
 	{
-	question1: "This is question one",
-	answer1: ["bears","beets","battlestar galactica"]
-	}//,
-	//{
-	//question2: "question 2",
-	//answer1: "answer 1",
-	//answer2: "answer 2",
-	//}
+	question1: "1. Which bear is best?",
+	answer1: ["bears","beets","battlestar galactica"],
+	currentAnswer: 2
+	
+	},
+	{
+	question2: "2. Which rhyming word is best?",
+	answer2: ["cats","rats","hats"],
+	currentAnswer: 0
 
+	}
 	//close questions
 	]     
     
@@ -26,9 +33,41 @@ var questions = [
     		$('#answer2').append(questions[0].answer1[1]).show();
     		$('#answer3').append(questions[0].answer1[2]).show();
     		$('#startbutton').hide();
-    		$('#submitbutton').show();
+    		$('#nextbutton1').show();
+
+
 
     })
+
+ 	$('#nextbutton1').on('click', function () {
+    if ($('input[name="optradio"]:checked').val() == questions[0].currentAnswer){
+	userScore.correct++;
+	} else{
+    userScore.incorrect++;
+	}
+	console.log(userScore);
+
+	$('#question').html(questions[1].question2);
+	$('#answer1').html(questions[1].answer2[0]);
+	$('#answer2').html(questions[1].answer2[1]);
+	$('#answer3').html(questions[1].answer2[2]);
+
+
+	})
+  
+
+   // if ($('input[name="optradio"]:checked').val() == currentAnswer){
+	//userScore.correct++;
+	//} else{
+   // userScore.incorrect++;
+	//}
+	//console.log(userScore);
+
+   // $('#submitbutton').on('click', function () {
+
+    
+
+   // })
 
 
 //end of document.ready function
