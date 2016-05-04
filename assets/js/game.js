2d8a326bec7861677b99c50ad4aef6165f65ffd2
Suzanne Kirk <sgraykirk@gmail.com>
2016-05-04 11:36:47 -0400

    before javascript questions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,31 @@
-function unhide(questions) {
-    var item = document.getElementById(questions);
-    if (item) {
-            item=(item=='hidden')?'unhidden':'hidden';
-        }
-       // document.getElementById(startbutton) = 'hidden';
-    }
+$(document).ready(function(){
+
+$('#questions').hide();
+$('#submitbutton').hide();
+
+	// var questions = [
+	//      {
+	//      	question: "question 1",
+	//      	answer1: "answer 1",
+	//      	answer2: "answer 2"
+	//      },
+	//      {
+	//      	question2: "question 2",
+	//      	answer1: "answer 1",
+	//      	answer2: "answer 2",
+	//      }
+
+	// ]     
+
+    $('#startbutton').on('click', function () {
+    		//$('#questions').append('something');
+    		$('#startbutton').hide();
+    		$('#questions').show();
+    		$('#submitbutton').show();
+
+    })
+
+
+
+})
+
